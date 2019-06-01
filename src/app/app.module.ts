d6adb262c92a0174f0ff6d2048f0323b1485f4dd
JAYA <jayabachi28@gmail.com>
2019-06-01 10:46:53 -0700

    package changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import{MatButtonModule,MatToolbarModule,,MatIconModule,MatCardModule,MatFormFieldModule,MatInputModule} from '@angular/';
+import { CitySearchComponent } from './city-search/city-search.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CitySearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    HttpClientModule,
+    BrowserAnimationModule,
+    FormModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatFormFileModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
